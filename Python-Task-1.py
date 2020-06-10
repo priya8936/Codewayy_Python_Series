593b6b2c87acb87a59cdd469336095f074674249
Priya Aggarwal <aggarwalpriya8936@gmail.com>
2020-06-10 20:58:40 +0530

    Python Task Added

---
+++ Python-Task-1.py
@@ -0,0 +1,18 @@
+#integer values
+a=10
+b=20
+print("SUM=", a+b)
+print("SUB=", a-b)
+print("MUL=", a*b)
+print("DIV=", a/b)
+print("MOD=", a%b)
+print()
+#float values
+c=20.5
+d=10.3
+print("SUM=", a+b)
+print("SUB=", a-b)
+print("MUL=", a*b)
+print("DIV=", a/b)
+print("MOD=", a%b)
+print()
